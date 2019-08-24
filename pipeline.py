783c581be18595a45ad4d603a2b79fb2b9b14bae
jpbreuer <jeanpaul.breuer@gmail.com>
2019-08-24 20:59:16 +0200

    constval=3

--- pipeline.py
+++ pipeline.py
@@ -286,7 +286,7 @@ def Preliminary_Products():
     
     if contourbin:
         file.write("echo 'Contour Binning'\n")
-        contbin = 'contbin --sn=' + str(sn_per_region) + ' --smoothsn=' + str(reg_smoothness) + ' --constrainfill --constrainval=2. scaled_broad_flux_fov_sps.fits\nmkdir contbin_sn' + str(sn_per_region) + '_smooth' + str(reg_smoothness) + '\nmkdir contbin_sn' + str(sn_per_region) + '_smooth' + str(reg_smoothness) + '/outreg\nmv bin_signal_stats.qdp bin_sn_stats.qdp contbin_binmap.fits contbin_mask.fits contbin_out.fits contbin_sn.fits contbin_sn' + str(sn_per_region) + '_smooth' + str(reg_smoothness) + '\ncd contbin_sn' + str(sn_per_region) + '_smooth' + str(reg_smoothness) + '\n'
+        contbin = 'contbin --sn=' + str(sn_per_region) + ' --smoothsn=' + str(reg_smoothness) + ' --constrainfill --constrainval=3. scaled_broad_flux_fov_sps.fits\nmkdir contbin_sn' + str(sn_per_region) + '_smooth' + str(reg_smoothness) + '\nmkdir contbin_sn' + str(sn_per_region) + '_smooth' + str(reg_smoothness) + '/outreg\nmv bin_signal_stats.qdp bin_sn_stats.qdp contbin_binmap.fits contbin_mask.fits contbin_out.fits contbin_sn.fits contbin_sn' + str(sn_per_region) + '_smooth' + str(reg_smoothness) + '\ncd contbin_sn' + str(sn_per_region) + '_smooth' + str(reg_smoothness) + '\n'
         mkregions = 'make_region_files --minx=' + str(minx) + ' --miny=' + str(miny) + ' --bin=1 --outdir=outreg contbin_binmap.fits\n'
         file.write(contbin)
         file.write(mkregions)
